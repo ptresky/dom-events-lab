de06f8c897bbccee49b45295d7f0a1802dc58c5b
ptresky <peter_tresky@hotmail.com>
2024-07-29 13:16:00 -0700

    add if statement for class types

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 /*-------------------------------- Constants --------------------------------*/
 const buttons = document.querySelectorAll('.button');
-
+const display = document.querySelector('.display')
 
 /*-------------------------------- Variables --------------------------------*/
-let display = document.querySelector('display')
 let num1 = 0;
 let num2 = 0;
 let operator = '';
+let resultDisplay = 0;
 
 /*------------------------ Cached Element References ------------------------*/
 
@@ -14,28 +14,55 @@ let operator = '';
 /*----------------------------- Event Listeners -----------------------------*/
 buttons.forEach((button) => {
     button.addEventListener('click', (event) => {
-      // This log is for testing purposes to verify we're getting the correct value
-      console.log(event.target.innerText);
-      // Future logic to capture the button's value would go here...
+      // This is where you would want to add control flow
+      // You need to direct the flow of you code based on which button is pressed
+      // HINT: think if statements
+
+      // You can achieve this be utilizing the classes of the DOM elements
+      // The event.target.classList.contains() you have a few lines down
+      // This method will return true or false 
+      if (event.target.classList.contains('number')){
+        display.innerText = ''};
+      if (event.target.classList.contains('operator')){
+
+      }
+        // code to dictate operations
+      if (event.target.classList.contains('equal')){
+        calculate()}
+
+      
+      
+        console.dir(event.target.classList);
+      console.dir(display.innerText + event.target.innerText)
+      display.innerText = display.innerText + event.target.innerText
+      if (event.target.classList === false);
+        calculate()
+      console.log(event.target.classList.contains('number'))
     });
+    //As a user, I want to be able to select numbers so that I can perform operations with them.
+    //when an operator is clicked, the calculator should stop displaying the clicks for num1 and begin num2
+    button.operator =  
   });
+  if (operator = 'C');
+    clearDisplay()
+  
   
 
 /*-------------------------------- Functions --------------------------------*/
 
 
-const solve = () => {
+const calculate = () => {
     if (operator === '+') {
-        (num1 + num2)
-    } 
+        resultDisplay = (num1 + num2)
+    } else
     if (operator === '-') {
-        (num1 - num2)
-    }
+        resultDisplay = (num1 - num2)
+    } else
     if (operator === '*') {
-        (num1 * num2)
-    }
+        resultDisplay = (num1 * num2)
+    } else
     if (operator === '/') {
-        (num1 /num2)
+        resultDisplay = (num1 /num2)
     }
 }
 const clearDisplay = () => {
@@ -47,4 +74,22 @@ select an operator
 capture the value of a second number
 solve the equation 
 clear the display
+*/
+
+/* const display = document.querySelector('.display');
+const button.number(input){
+display.value += input;
+}
+
+const clearDisplay(){
+display.valye = '';
+}
+
+const calculate(){
+try(display.valye - eval(display.valye);
+)
+switch(error){
+display.value = "error"};
+
+}
 */
